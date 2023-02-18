c1c9ee961f4123f5f9c08115b74d1c91e61d0d29
Vladimir Samsonov <vnsamsonov@gmail.com>
2023-02-18 23:54:16 +0500

    feat: add parse vault path to secret
    New version include parse full path to keys of the secret.

--- k8s-itlabs-operator/clients/vault/parse.py
+++ k8s-itlabs-operator/clients/vault/parse.py
@@ -1,3 +1,4 @@
+import re
 from collections import namedtuple
 
 import hvac
@@ -5,6 +6,8 @@ import hvac
 from clients.vault.specifications import VAULT_SCHEME
 
 SecretPathParseResult = namedtuple("VaultParseResult", "mount_point, path")
+
+VaultPath = namedtuple("VaultPath", "mount_point, path, key")
 
 
 def parse_secret_path(secret_path: str) -> SecretPathParseResult:
@@ -24,3 +27,30 @@ def parse_secret_path(secret_path: str) -> SecretPathParseResult:
     if not (mount_point and path):
         raise hvac.v1.exceptions.InvalidPath("Invalid vault path")
     return SecretPathParseResult(mount_point=mount_point, path=path[0])
+
+
+def parse_vault_path(vault_path: str) -> VaultPath:
+    """
+    Parse vault path to secret
+
+    Vault secret path scheme:
+        vault:{mount_point}/data/{path}[#KEY]
+
+    Example:
+        >>> parse_vault_path('vault:secret/data/application/postgres')
+        VaultPath(mount_point='secret', path='application/postgres', key=None)
+
+        >>> parse_vault_path('vault:secret/data/application/postgres#HOST')
+        VaultPath(mount_point='secret', path='application/postgres', key='HOST')
+    """
+
+    scheme = r"vault"
+    mount_point = r"(?P<mount_point>\w+)"
+    path = r"(?P<path>\w+(/\w+)+)"
+    key = r"(?P<key>\w+)"
+
+    regex = f"^{scheme}:{mount_point}/data/{path}(#{key})?$"
+    match = re.match(regex, vault_path)
+    if match is None:
+        raise hvac.v1.exceptions.InvalidPath("Invalid vault path scheme")
+    return VaultPath(**match.groupdict())
--- k8s-itlabs-operator/clients/vault/tests/parse_test.py
+++ k8s-itlabs-operator/clients/vault/tests/parse_test.py
@@ -1,7 +1,11 @@
 import hvac
 import pytest
 
-from clients.vault.parse import SecretPathParseResult, parse_secret_path
+from clients.vault.parse import \
+    SecretPathParseResult, \
+    VaultPath, \
+    parse_secret_path, \
+    parse_vault_path
 
 
 @pytest.mark.parametrize(
@@ -32,3 +36,35 @@ def test_parse_secret_path(secret_path, expected):
 def test_error_for_invalid_vault_path(secret_path):
     with pytest.raises(hvac.v1.exceptions.InvalidPath):
         parse_secret_path(secret_path)
+
+
+@pytest.mark.parametrize(
+    "vault_path, expected",
+    (
+        (
+            "vault:secret/data/application/postgres",
+            VaultPath(mount_point="secret", path="application/postgres", key=None),
+        ),
+        (
+            "vault:secret/data/application/postgres#HOST",
+            VaultPath(mount_point="secret", path="application/postgres", key="HOST"),
+        ),
+    )
+)
+def test_parse_vault_path(vault_path, expected):
+    assert parse_vault_path(vault_path) == expected
+
+
+@pytest.mark.parametrize(
+    "vault_path",
+    (
+        "application/postgres",
+        "vault://data/application/postgres",
+        "vault:data/application/postgres",
+        "vault:secret/application/postgres",
+        "vault:secret/application/postgres#HOST#PORT",
+    ),
+)
+def test_raise_error_on_invalid_path(vault_path):
+    with pytest.raises(hvac.v1.exceptions.InvalidPath):
+        parse_vault_path(vault_path)
